52153ecd6c5f83c9cac0e849ec6ab296e6d158e5
Adriann Guedes <adrianguedes902@gmail.com>
2024-05-14 22:18:55 -0300

    começando tela cadastro-usuário

---
+++ gtfrontend/src/routes/cadastro/cadastro.js
@@ -0,0 +1,14 @@
+import Cabecario from "../../components/cabecario/cabecario";
+
+function Cadastro() {
+    return (
+        <>
+        <Cabecario/>
+            <div className="container">
+                
+            </div></>
+
+    );
+}
+
+export default Cadastro;
