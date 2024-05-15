fd9382bc9f4de20673ab6e848f08d82a251bdfa1
Adriann Guedes <adrianguedes902@gmail.com>
2024-05-14 22:18:11 -0300

    configurando react-router-dom

--- gtfrontend/src/index.js
+++ gtfrontend/src/index.js
@@ -4,14 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Login from './routes/login/login';
+import Cadastro from './routes/cadastro/cadastro';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App/>,
+    children:[
+      {
+        path: "/",
+        element: <Login/>
+      },
+      {
+        path: "/cadastro",
+        element: <Cadastro/>
+      }
+
+    ]
+  },
+])
+  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
